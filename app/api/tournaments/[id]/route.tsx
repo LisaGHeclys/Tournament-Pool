3db7c11d03ac4842d395375fab2c42bd223894ae
Lisa Glaziou <lisa.glaziou@epitech.eu>
2024-10-12 12:14:46 +0200

    wip: api routes for the tournaments

--- app/api/tournaments/[id]/route.tsx
+++ app/api/tournaments/[id]/route.tsx
@@ -4,18 +4,36 @@ import withSession from "@/app/api/_helpers/middleware/with-session";
 import { tournamentBody } from "@/app/api/_helpers/types/interfaces";
 import { Session } from "next-auth";
 
-async function getHandler(
-  req: NextRequest,
-  session?: Session,
-  argument?: string,
-) {
-  if (!argument)
-    return NextResponse.json({ error: "Internal error" }, { status: 500 });
+async function getHandler(req: NextRequest, session?: Session, id?: string) {
+  if (!id)
+    return NextResponse.json({ error: "Id is required" }, { status: 500 });
 
   try {
     const tournament = await getDb()
       .collection("tournaments")
-      .doc(argument)
+      .doc(id)
+      .get()
+      .then((doc) => ({ id: doc.id, ...doc.data() }) as tournamentBody);
+
+    if (!tournament)
+      return NextResponse.json(
+        { error: "Tournament not found" },
+        { status: 404 },
+      );
+
+    return NextResponse.json({ ...tournament });
+  } catch (e) {
+    return NextResponse.json({ error: e }, { status: 500 });
+  }
+}
+
+async function deleteHandler(req: NextRequest, session?: Session, id?: string) {
+  if (!id) return NextResponse.json({ error: "id error" }, { status: 500 });
+
+  try {
+    const tournament = await getDb()
+      .collection("tournaments")
+      .doc(id)
       .get()
       .then((doc) => ({ id: doc.id, ...doc.data() }) as tournamentBody);
 
@@ -32,6 +50,20 @@ async function getHandler(
 }
 
 export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  return withSession(req, getHandler, params.id);
+}
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  return withSession(req, getHandler, params.id);
+}
+
+export async function DELETE(
   req: NextRequest,
   { params }: { params: { id: string } },
 ) {
