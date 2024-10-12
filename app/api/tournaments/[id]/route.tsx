aeaf47e37b61df8e9cb2d8ab74119e9a1eb617ce
Lisa Glaziou <lisa.glaziou@epitech.eu>
2024-10-12 17:46:35 +0200

    fix: delete tournament by id route

--- app/api/tournaments/[id]/route.tsx
+++ app/api/tournaments/[id]/route.tsx
@@ -2,9 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import { getDb } from "@/lib/firebase/firebase";
 import withSession from "@/app/api/_helpers/middleware/with-session";
 import { tournamentBody } from "@/app/api/_helpers/types/interfaces";
-import { Session } from "next-auth";
 
-async function getHandler(req: NextRequest, session?: Session, id?: string) {
+async function getHandler(req: NextRequest) {
+  const params = req.nextUrl.searchParams;
+  const id = params.get("id");
+
   if (!id)
     return NextResponse.json({ error: "Id is required" }, { status: 500 });
 
@@ -27,45 +29,29 @@ async function getHandler(req: NextRequest, session?: Session, id?: string) {
   }
 }
 
-async function deleteHandler(req: NextRequest, session?: Session, id?: string) {
-  if (!id) return NextResponse.json({ error: "id error" }, { status: 500 });
+async function deleteHandler(req: NextRequest) {
+  const params = req.nextUrl.searchParams;
+  const id = params.get("id");
+
+  if (!id)
+    return NextResponse.json({ error: "Id is required" }, { status: 500 });
 
   try {
-    const tournament = await getDb()
-      .collection("tournaments")
-      .doc(id)
-      .get()
-      .then((doc) => ({ id: doc.id, ...doc.data() }) as tournamentBody);
-
-    if (!tournament)
-      return NextResponse.json(
-        { error: "Tournament not found" },
-        { status: 404 },
-      );
-
-    return NextResponse.json({ ...tournament });
+    await getDb().collection("tournaments").doc(id).delete();
+    return NextResponse.json({});
   } catch (e) {
     return NextResponse.json({ error: e }, { status: 500 });
   }
 }
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { id: string } },
-) {
-  return withSession(req, getHandler, params.id);
+export async function GET(req: NextRequest) {
+  return withSession(req, getHandler);
 }
 
-export async function POST(
-  req: NextRequest,
-  { params }: { params: { id: string } },
-) {
-  return withSession(req, getHandler, params.id);
+export async function POST(req: NextRequest) {
+  return withSession(req, getHandler);
 }
 
-export async function DELETE(
-  req: NextRequest,
-  { params }: { params: { id: string } },
-) {
-  return withSession(req, getHandler, params.id);
+export async function DELETE(req: NextRequest) {
+  return withSession(req, deleteHandler);
 }
