ab2afd8318171a579617a6bf31044edd865dfc44
Lisopaïne <72012368+LisaGHeclys@users.noreply.github.com>
2024-10-22 11:42:19 +0200

    33 fix padding on all the pages (#37)
    * fix: footer display
    
    * fix: padding on all the different pages

--- app/page.tsx
+++ app/page.tsx
@@ -107,7 +107,7 @@ export default function Home() {
 
   if (isLoading) {
     return (
-      <div className="min-h-screen  sm:p-16 p-8 gap-6 grid sm:grid-rows-[20px_1fr_20px] items-center sm:justify-items-center font-[family-name:var(--font-geist-sans)]">
+      <div className="min-h-screen sm:p-16 p-8 gap-6 grid sm:grid-rows-[20px_1fr_20px] items-center sm:justify-items-center font-[family-name:var(--font-geist-sans)]">
         <header className="p-8 w-full h-fit flex flex-wrap items-center sm:flex-row justify-between">
           <div />
           <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
