0d8680c1c0536f61d704728d38ff8120011afe90
Lisa Glaziou <lisa.glaziou@epitech.eu>
2024-11-04 11:23:48 +0100

    fix: login page responsive

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,6 +11,7 @@ import {
 import { Github } from "lucide-react";
 import React from "react";
 import { signIn, useSession } from "next-auth/react";
+import Footer from "@/components/ui/footer";
 
 export default function Home() {
   const router = useRouter();
@@ -25,7 +26,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen p-8 sm:pb-20 gap-16 grid sm:grid-rows-[20px_1fr_20px] items-center sm:justify-items-center sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header className="p-8 w-full h-fit flex flex-wrap sm:flex-row items-center justify-between">
+      <header className="md:p-8 w-full md:h-fit flex flex-col md:flex-row md:items-center justify-between">
         <Button
           className="hover:scale-105 transition ease-in-out delay-150"
           onClick={() => {
@@ -34,13 +35,13 @@ export default function Home() {
         >
           Homepage
         </Button>
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        <h1 className="text-2xl md:text-4xl font-extrabold tracking-tight lg:text-5xl">
           Welcome to the Login page !
         </h1>
         <div />
       </header>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Card className="w-[400px]">
+      <main className="w-full flex flex-col gap-8 justify-center items-center">
+        <Card className="w-full md:w-[400px]">
           <CardHeader>
             <CardTitle>Login</CardTitle>
             <CardDescription>
@@ -60,6 +61,7 @@ export default function Home() {
           </CardContent>
         </Card>
       </main>
+      <Footer />
     </div>
   );
 }
