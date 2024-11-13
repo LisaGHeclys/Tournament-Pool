eaaa51741b72c06933423f23c2ddb800d9be2805
Lisa Glaziou <lisa.glaziou@epitech.eu>
2024-11-13 17:36:11 +0100

    fix: toggle theme hover button

--- components/ui/navbar/theme/theme-toggle.tsx
+++ components/ui/navbar/theme/theme-toggle.tsx
@@ -20,7 +20,7 @@ export function ThemeToggle() {
         <Button
           variant="outline"
           size="icon"
-          className="rounded-full h-11 w-11"
+          className="rounded-full h-11 w-11 hover:scale-110 transition ease-in-out delay-250"
         >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
