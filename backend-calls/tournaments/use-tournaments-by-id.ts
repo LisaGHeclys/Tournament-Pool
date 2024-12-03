bdfc6924df2b4bba025f9811542947d157544a91
Lisa Glaziou <lisa.glaziou@epitech.eu>
2024-12-03 09:30:04 +0100

    rm: unused code

--- backend-calls/tournaments/use-tournaments-by-id.ts
+++ backend-calls/tournaments/use-tournaments-by-id.ts
@@ -10,10 +10,6 @@ type Props = {
 export function useTournamentsById({ id, refetchInterval }: Props) {
   async function getTournamentsByIdFn(id: string) {
     const response = await apiClient.get(`/tournaments/${id}`);
-    // response.data.points =
-    //   response.data.points.length > 45 && isShow
-    //     ? response.data.points.slice(0, 45)
-    //     : response.data.points;
     return response.data as tournamentBody;
   }
   return useQuery({
