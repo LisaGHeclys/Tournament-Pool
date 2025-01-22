4d575cefd754a30e17dfe5480218ee83e73804ea
Lisa Glaziou <lisa.glaziou@epitech.eu>
2025-01-22 19:25:13 +0100

    add: a sidebar

--- app/(views)/[locale]/(protected)/admin/layout.tsx
+++ app/(views)/[locale]/(protected)/admin/layout.tsx
@@ -1,5 +1,8 @@
+"use client";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/ui/app-sidebar";
 
 export default function ProtectedLayout({
   children,
@@ -10,16 +13,13 @@ export default function ProtectedLayout({
 
   if (session?.user?.email === process.env.NEXT_PUBLIC_ADMIN_EMAIL) {
     return (
-      <div className="min-h-screen sm:p-16 p-8 gap-6 grid sm:grid-rows-[20px_1fr_20px] items-center sm:justify-items-center font-[family-name:var(--font-geist-sans)]">
-        <header className="p-8 w-full h-fit flex flex-wrap sm:flex-row items-center justify-between">
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-            Admin Hi !
-          </h1>
-        </header>
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
           {children}
         </main>
-      </div>
+      </SidebarProvider>
     );
   } else redirect("/");
 }
