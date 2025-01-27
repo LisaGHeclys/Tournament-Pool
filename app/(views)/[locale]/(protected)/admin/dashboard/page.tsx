47a5ab871e66112561441a0c4c6a3c7560722c37
Lisa Glaziou <lisa.glaziou@epitech.eu>
2025-01-27 17:30:06 +0100

    wip: grid template

--- app/(views)/[locale]/(protected)/admin/dashboard/page.tsx
+++ app/(views)/[locale]/(protected)/admin/dashboard/page.tsx
@@ -8,7 +8,13 @@ export default function Dashboard() {
   return (
     <div>
       <Header to={`${locale}/admin/dashboard`} breadcrumbName={"Dashboard"} />
-      dashboard
+      <div className="px-8 py-4 grid gap-4">
+        <div className="grid grid-cols-2 gap-4 w-full">
+          <div className="bg-gray-200 p-4">First Element (50%)</div>
+          <div className="bg-gray-200 p-4">Second Element (50%)</div>
+        </div>
+        <div className="bg-gray-200 p-4">Third Element (100%)</div>
+      </div>
     </div>
   );
 }
