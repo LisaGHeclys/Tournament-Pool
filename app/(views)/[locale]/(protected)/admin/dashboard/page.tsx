c74095eeb0a61598792b67b038027f8a98f9d4ed
Lisa Glaziou <lisa.glaziou@epitech.eu>
2025-01-28 16:57:45 +0100

    fix: shadow of cards

--- app/(views)/[locale]/(protected)/admin/dashboard/page.tsx
+++ app/(views)/[locale]/(protected)/admin/dashboard/page.tsx
@@ -34,21 +34,21 @@ export default function Dashboard() {
     <div className="flex flex-col h-screen">
       <Header to={`${locale}/admin/dashboard`} breadcrumbName={"Dashboard"} />
       <div className="flex-1 px-8 pb-8 pt-4 grid grid-rows-3 grid-cols-2 gap-4">
-        <div className="row-span-2 shadow-2xl rounded-2xl">
+        <div className="row-span-2 shadow-xl rounded-2xl">
           {latestTournamentsQuery.isPending ? (
             <Skeleton className="h-full w-full flex rounded-2xl" />
           ) : (
             <Skeleton className="h-full w-full flex rounded-2xl" />
           )}
         </div>
-        <div className="row-span-2 shadow-2xl rounded-2xl">
+        <div className="row-span-2 shadow-xl rounded-2xl">
           {latestTournamentsQuery.isPending ? (
             <Skeleton className="h-full w-full flex rounded-2xl" />
           ) : (
             <Skeleton className="h-full w-full flex rounded-2xl" />
           )}
         </div>
-        <div className="col-span-2 shadow-2xl rounded-2xl">
+        <div className="col-span-2 shadow-xl rounded-2xl">
           {latestTournamentsQuery.isPending ? (
             <Skeleton className="h-full w-full flex rounded-2xl" />
           ) : (
